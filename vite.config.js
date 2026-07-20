ed248ddedb6e2ed3f0357c3c16c820421795f6d3
Design <darkman_anubis_iara@hotmail.com>
2026-07-20 01:47:57 -0300

    Tercer commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return {
+    base: '/MultiService/',
     build: {
       rollupOptions: {
         input: {
