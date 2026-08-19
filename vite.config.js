161308be2d546075525c278ad3ad280f91aa2bcb
Design <darkman_anubis_iara@hotmail.com>
2026-08-19 18:39:01 -0300

    Reparamos Responsive para tablet de 7 a 10

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: '/',
+
+    //para ver el proyecto en la red local, para poder acceder desde otros dispositivos
+    server: {
+      host: true,      // escucha en 0.0.0.0 (todas las IPs)
+      port: 5173,
+    },
+    
     build: {
       rollupOptions: {
         input: {
